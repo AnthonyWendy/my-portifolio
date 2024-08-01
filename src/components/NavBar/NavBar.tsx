8e78a6d9a25a2046ec55cd1d7a8e25b547b43b82
AnthonyWendy <anthonywendyantunesdelima@gmail.com>
2024-08-01 07:38:07 -0300

    feat: creates navbar and syled

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,21 @@
+import { AppBar, MenuItem, Toolbar } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const NavBar = () => {
+  const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "space-evenly",
+  }));
+
+  return (
+    <AppBar position="absolute">
+      <StyledToolbar>
+        <MenuItem>About</MenuItem>
+        <MenuItem>Skill</MenuItem>
+        <MenuItem>Contact</MenuItem>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
