f8ff4a926912803649d05ce211138fafd97caa4d
AnthonyWendy <anthonywendyantunesdelima@gmail.com>
2024-08-01 07:51:39 -0300

    fix: create small functions to buttons

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import { AppBar, MenuItem, Toolbar } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 const NavBar = () => {
-  const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+  const StyledToolbar = styled(Toolbar)(() => ({
     display: "flex",
     justifyContent: "space-evenly",
   }));
